61e04fef01f0f7be91d0f72605e3838fb731c65c
Daemondeal <daemondeal@gmail.com>
2018-11-21 21:17:24 +0100

    Added basic functions

---
+++ waves_viewer.js
@@ -0,0 +1,33 @@
+var matches = "";
+var waves = [];
+
+$(document).ready(function(){
+    loadJSON();
+
+    $('#searchbox').keyup(function(){
+        populateUl(search(this.value));
+        
+    });
+});
+
+function loadJSON(){
+    $.getJSON('./waves.json', function(data){
+        waves = data;
+        var v = $("#searchbox").val();
+        populateUl(search(v));
+    });
+}
+
+function search(query){
+    return waves.filter(function(x){
+        return x.name.includes(query);
+    });//.slice(0, 10);
+}
+
+function populateUl(results){
+    var list = $('.results-list');
+    list.empty();
+    for(var i of results){
+        list.append(`<li>${i.name}: ${i.cost}</li>`);
+    }
+}
