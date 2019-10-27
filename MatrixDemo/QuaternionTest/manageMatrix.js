21c631c55cbe1cc7e9261d6b8875a95ebfb6d363
Daemond <daemondeal@gmail.com>
2019-10-27 18:31:53 +0100

    Changed Drawing

--- MatrixDemo/QuaternionTest/manageMatrix.js
+++ MatrixDemo/QuaternionTest/manageMatrix.js
@@ -40,6 +40,6 @@ function setMatrix(matrix){
 
 window.addEventListener('load', () => {
   setMatrix([
-    [3, 0, 0], [0, 3, 0], [0, 0, 3]
+    [0.5, 0, 0], [0, 0.5, 0], [0, 0, 0.5]
   ])
 }, false);
